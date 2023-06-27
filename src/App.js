64cf35d05406aaac5def88bab853165ed719ea57
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-06-27 20:12:59 +0200

    homepage is under the creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import HomePage from "./pages/homepage/HomePage";
 import Topbar from "./topbar/Topbar";
+
 
 
 function App() {
   return (
     <div className="App">
      <Topbar/>
+     <HomePage/>
     </div>
   );
 }
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -2,6 +2,12 @@ import "./header.css"
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="headerTitles">
+            <span className="headerTitleSmall">Javascript & Node.js</span>
+            <span className="headerTitleLarge">JS Blog</span>
+        </div>
+        {/* <img className="headerImg" src="../Assets/images/JavascriptBanner.jpg " alt="dddd" /> */}
+    </div>
   )
 }
