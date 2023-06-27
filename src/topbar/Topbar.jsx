8876d01b4e3bc17cb26b1f622a36afc717b72dc5
Mohammad Ghasempour <m.gh84nor@gmail.com>
2023-06-27 19:35:07 +0200

    topbar component finished

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -3,7 +3,28 @@ import "./topbar.css"
 export default function Topbar() {
   return (
     <>
-    <div className="top">Topbar</div>
+    <div className="top">
+      <div className="topLeft">
+      <i className="topIcon fa-brands fa-square-twitter"></i>
+      <i className="topIcon fa-brands fa-square-pinterest"></i>
+      <i className="topIcon fa-brands fa-square-facebook"></i>
+      <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">CATEGORIES</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImage" src="https://avatars.githubusercontent.com/u/89663327?v=4" alt="" />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
 
     </>
   )
